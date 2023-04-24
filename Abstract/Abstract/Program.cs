641cfcb77aaf46d6f7eb888a2b851d30f9008f1f
adlarajasri <129931618+adlarajasri@users.noreply.github.com>
2023-04-24 09:22:55 +0530

    Add files via upload

---
+++ Abstract/Abstract/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Abstract
+{
+    abstract class Figure
+    {
+        public int Dimension;
+        public abstract double Area();
+        public abstract double Perimeter();
+    }
+    class Square : Figure
+    {
+        public override double Area()
+        {
+            return Dimension * Dimension;
+        }
+        public override double Perimeter()
+        {
+            return 4 * Dimension; 
+        }
+    }
+    class Circle : Figure
+    {
+        public override double Area()
+        {
+            return Math.PI*Dimension*Dimension;
+        }
+        public override double Perimeter()
+        {
+            return 2*Math.PI*Dimension;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Figure f = new Square();        //Upcasting
+            f.Dimension = 10;
+            Console.WriteLine(f.Area());
+            Console.WriteLine(f.Perimeter());
+            Figure f1 = new Circle();        //Upcasting
+            f1.Dimension = 20;
+            Console.WriteLine(f1.Area());
+            Console.WriteLine(f1.Perimeter());
+
+        }
+    }
+}
